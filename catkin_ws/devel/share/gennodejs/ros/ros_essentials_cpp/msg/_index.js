4412bc7f17cacd6d827b76595d07b4fd524977b6
Daniel Bogdoll <daniel.bogdoll@rwth-aachen.de>
2020-08-25 22:13:15 +0200

    Finished setup

---
+++ catkin_ws/devel/share/gennodejs/ros/ros_essentials_cpp/msg/_index.js
@@ -0,0 +1,22 @@
+
+"use strict";
+
+let IoTSensor = require('./IoTSensor.js');
+let FibonacciGoal = require('./FibonacciGoal.js');
+let FibonacciActionFeedback = require('./FibonacciActionFeedback.js');
+let FibonacciFeedback = require('./FibonacciFeedback.js');
+let FibonacciActionResult = require('./FibonacciActionResult.js');
+let FibonacciResult = require('./FibonacciResult.js');
+let FibonacciAction = require('./FibonacciAction.js');
+let FibonacciActionGoal = require('./FibonacciActionGoal.js');
+
+module.exports = {
+  IoTSensor: IoTSensor,
+  FibonacciGoal: FibonacciGoal,
+  FibonacciActionFeedback: FibonacciActionFeedback,
+  FibonacciFeedback: FibonacciFeedback,
+  FibonacciActionResult: FibonacciActionResult,
+  FibonacciResult: FibonacciResult,
+  FibonacciAction: FibonacciAction,
+  FibonacciActionGoal: FibonacciActionGoal,
+};
