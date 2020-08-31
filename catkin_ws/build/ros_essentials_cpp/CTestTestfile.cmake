66d04331dc14bdbc12cd035e17e93b6a461532be
Daniel Bogdoll <daniel.bogdoll@saym.io>
2020-08-31 09:33:28 +0200

    Example as folder instead of git repo

--- catkin_ws/build/ros_essentials_cpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniel/ros/catkin_ws/src/ros_essentials_cpp
-# Build directory: /home/daniel/ros/catkin_ws/build/ros_essentials_cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/build/ros_essentials_cpp/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/daniel/ros/catkin_ws/devel/include;/home/daniel/ros/catkin_ws/src/ros_essentials_cpp/include".split(';') if "/home/daniel/ros/catkin_ws/devel/include;/home/daniel/ros/catkin_ws/src/ros_essentials_cpp/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs;message_runtime".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lros_essentials_cpp".split(';') if "-lros_essentials_cpp" != "" else []
-PROJECT_NAME = "ros_essentials_cpp"
-PROJECT_SPACE_DIR = "/home/daniel/ros/catkin_ws/devel"
-PROJECT_VERSION = "0.0.0"
