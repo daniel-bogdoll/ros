7db4d8d40bbe54a948df79582ffb97c51e22a71c
Daniel Bogdoll <daniel.bogdoll@saym.io>
2020-08-31 13:54:34 +0200

    Added missing files

---
+++ catkin_ws/build/ros_basics_tutorials/CMakeFiles/listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/daniel/ros/catkin_ws/devel/lib/ros_basics_tutorials/listener"
+  "/home/daniel/ros/catkin_ws/devel/lib/ros_basics_tutorials/listener.pdb"
+  "CMakeFiles/listener.dir/src/listener.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/build/ros_basics_tutorials/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/daniel/ros/catkin_ws/devel/lib/ros_basics_tutorials/talker"
+  "/home/daniel/ros/catkin_ws/devel/lib/ros_basics_tutorials/talker.pdb"
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
