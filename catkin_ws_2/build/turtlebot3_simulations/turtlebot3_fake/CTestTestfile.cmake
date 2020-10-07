8c699bbd33ad5b337fe5d01015884a10fbc20806
Daniel Bogdoll <daniel.bogdoll@rwth-aachen.de>
2020-10-07 12:02:22 +0200

    Turtlebot 3 simulator working

---
+++ catkin_ws_2/build/turtlebot3_simulations/turtlebot3_fake/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/daniel/ros/catkin_ws_2/src/turtlebot3_simulations/turtlebot3_fake
+# Build directory: /home/daniel/ros/catkin_ws_2/build/turtlebot3_simulations/turtlebot3_fake
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
