d790dfff0636a08affa5a756efd4109145646a24
Daniel Bogdoll <daniel.bogdoll@rwth-aachen.de>
2020-10-08 13:36:50 +0200

    Fixed file path for house map

---
+++ catkin_ws_2/build/ros_course_part2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/daniel/ros/catkin_ws_2/src/ros_course_part2
+# Build directory: /home/daniel/ros/catkin_ws_2/build/ros_course_part2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
